b388332d14c478420782767504da767ea6a67740
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-02 22:38:46 -0300

    por favor me libera

---
+++ src/main/java/com/example/artifactdemo/NamedemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.artifactdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/artifactdemo/NamedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artifactdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
